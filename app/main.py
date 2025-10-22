ca4811d62c8ea41970f6b084e2eada290664e136
alok-raj <Alok.Raj@mindfiresolutions.com>
2025-10-22 16:57:40 +0530

    feat(server): add user modal

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
-from app.config.dbconf import SessionLocal
+from app.config.dbconf import SessionLocal, engine
+from app.models import user_model
+from sqlalchemy import text
+
 app = FastAPI(title="User CRUD API")
-# app.include_router(user_routes.router)
+
+user_model.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
     db = SessionLocal()
-    print("db",db.connection)
-    return {"message": "User CRUD API is running!"}
+    try:
+        result = db.execute(text("SELECT 1")).scalar()
+        print("DB connected, test query result:", result)
+        return {"message": "User CRUD API is running!"}
+    finally:
+        db.close()
